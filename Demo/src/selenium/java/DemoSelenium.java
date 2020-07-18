931b04a04b43639c23f6ee454362902d163e0abe
unknown <shaktiSDET>
2020-07-18 19:24:30 +0530

    second commit

--- Demo/src/selenium/java/DemoSelenium.java
+++ Demo/src/selenium/java/DemoSelenium.java
@@ -1,16 +1,18 @@
 package selenium.java;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DemoSelenium {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		String s="s";
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\hasher\\Downloads\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.youtube.com/");
-
+		Thread.sleep(2000);
+		driver.findElement(By.id("guide-icon")).click();
 	}
 
 }
