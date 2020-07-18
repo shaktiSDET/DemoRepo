b3a8d3cc49a4f5af21fa148aebc439fdaf6f7b3d
unknown <shaktiSDET>
2020-07-18 19:04:05 +0530

    updatedDemo/src/selenium/java/DemoSelenium.java

--- Demo/src/selenium/java/DemoSelenium.java
+++ Demo/src/selenium/java/DemoSelenium.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class DemoSelenium {
 
 	public static void main(String[] args) {
-		
+		String s="s";
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\hasher\\Downloads\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.youtube.com/");
