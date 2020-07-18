75b3e9b43042d317264910a1cbef97b0216f154d
Shakti Mohanty <59830397+shaktiSDET@users.noreply.github.com>
2020-07-18 18:59:52 +0530

    Add files via upload

---
+++ Demo/src/selenium/java/DemoSelenium.java
@@ -0,0 +1,16 @@
+package selenium.java;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DemoSelenium {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\hasher\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.youtube.com/");
+
+	}
+
+}
